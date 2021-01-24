8b52897f1857e95eaced74c77c0a20325d13ed5b
Dicmen92 <58484766+Dicmen92@users.noreply.github.com>
2021-01-24 22:38:01 +0600

    Add files via upload

---
+++ 5/src/js/script.js
@@ -0,0 +1,8 @@
+const arr = new Array();
+//const arr = [];
+for (let i=0, t=20; i<t; i++) {
+    arr.push(Math.round(Math.random() * t))    
+}
+document.write(arr);
+
+//Массив получилось заполнить только случайными числами..
